592cdd43a147672d2d3ca91c1735cb8aea80a3a1
Ellion Blessan <ellionblessan@gmail.com>
2021-11-17 17:13:21 +0700

    Penambahan CRUD

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,19 @@ Route::get('praktikum2', function () {
 
 Route::get('ets2021', "ViewController@showETS");
 
-Route::get('tugasphp', "ViewController@showTugasPHP");
+Route::get('tugasphp',"ViewController@tugasPHP");
 
 Route::post('results',"ViewController@showResults");
 
-Route::get('tugasphp',"ViewController@tugasPHP");
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
